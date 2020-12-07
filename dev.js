22987bd0f5b7d5872bc96b54843a3d5c4d49e692
SparkleX <xufei.sun@sap.com>
2020-12-07 11:03:42 +0800

    dev

---
+++ dev.js
@@ -0,0 +1,9 @@
+const Koa = require("koa");
+const KoaLogger = require("koa-logger");
+const KoaMount = require("koa-mount");
+const KoaStatic  = require("koa-static");
+const app = new Koa();
+app.use(KoaLogger());
+app.use(KoaMount("/web", KoaStatic("src")));
+const port = process.env.PORT || 3000;
+app.listen(port);
