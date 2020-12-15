abfd93bf5576c0b273b6ddce07489274f7b53a44
SparkleX <xufei.sun@sap.com>
2020-12-15 17:01:16 +0800

    dev

--- src/help/01/Component.js
+++ src/help/01/Component.js
@@ -1,28 +1,18 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel"
-], function (UIComponent, JSONModel) {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"], function (UIComponent, JSONModel) {
 	"use strict";
-
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-
-		metadata : {
+		metadata: {
 			manifest: "json"
 		},
-
-		init : function () {
-			// call the init function of the parent
+		init: function () {
 			UIComponent.prototype.init.apply(this, arguments);
-
-			// set data model
 			var oData = {
-				recipient : {
-					name : "World"
+				recipient: {
+					name: "World"
 				}
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 		}
 	});
-
 });
